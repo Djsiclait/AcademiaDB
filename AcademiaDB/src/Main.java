0985108520f44e98e3ea32bddcd12e2474d240e3
djsiclait <djidjellypas92@gmail.com>
2016-05-24 01:35:14 -0400

    Create student class

--- AcademiaDB/src/Main.java
+++ AcademiaDB/src/Main.java
@@ -11,11 +11,11 @@ public class Main {
 
     public static void main(String[] args) throws Exception{
 
-        System.out.println("Ping!");
+        System.out.println("Ping 1!");
         //Class.forName("org.h2.Driver");
         //Connection conn = DriverManager.getConnection("jdbc:h2:~/Estudiantes", "sa", "");
 
-        System.out.println("Ping!");
+        System.out.println("Ping 2!");
 
         //conn.close();
     }
---
+++ AcademiaDB/src/Student.java
@@ -0,0 +1,22 @@
+/**
+ * Created by Siclait on 5/24/2016.
+ */
+public class Student {
+
+    // Attributes
+    public int matricula;
+    public String name;
+    public String lastName;
+    public String telephone;
+
+    public Student(){
+
+    }
+
+    public Student(int matricula, String name, String lastName, String telephone){
+        this.matricula = matricula;
+        this.name = name;
+        this.lastName = lastName;
+        this.telephone = telephone;
+    }
+}
