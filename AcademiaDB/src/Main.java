c08fa20ef598854d28c1608a3998914ff9aca724
djsiclait <djidjellypas92@gmail.com>
2016-05-24 00:10:43 -0400

    Add AcademiaDB java project: (H2 ERRORgit status git status )

---
+++ AcademiaDB/src/Main.java
@@ -0,0 +1,20 @@
+/**
+ * Created by Siclait on 5/23/2016.
+ */
+
+// Libraries
+import java.sql.*;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception{
+
+        System.out.println("Ping!");
+        //Class.forName("org.h2.Driver");
+        //Connection conn = DriverManager.getConnection("jdbc:h2:~/Estudiantes", "sa", "");
+
+        System.out.println("Ping!");
+
+        //conn.close();
+    }
+}
